ce5d83064563f52f67903e9e0b41a771071572a6
JoAdrian <50709946+JoAdrian@users.noreply.github.com>
2019-11-29 22:20:57 -0300

    Mudança
    Algumas funções refeitas, como as que retornam posições. Mudança no tipo de dados. Alguns construtores a mais.

--- pos.h
+++ pos.h
@@ -2,30 +2,38 @@
 #define POS_H
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 class Pos
 {
 public:
 
-    char lin; //A a J
-    char col; //0 a 1
+    int lin; //0 a 9
+    int col; //0 a 9
 
     ///CONSTRUTORES
-    inline Pos():lin(' '),col(' '){}
-    inline Pos(const Pos& p):lin(p.lin),col(p.col){}
+    inline Pos():lin(-1),col(-1){}
+    inline Pos(const Pos& p):lin(p.lin),col(p.col){;}
+    inline Pos(int l, int c){
+        if((l >= 0 && l <= 9) && (l >= 0 && l <= 9)){
+            lin = l;
+            col = c;
+        }
+    }
+    Pos(string &s);
     Pos(char l, char c);
-    //falta o por string
 
-    bool isLinValid(char c);
-    bool isColValid(char c);
-    bool isValid(const Pos p);
-    Pos nextVertical(const Pos p); //se n for valido ele retorna o mesmo valor
-    Pos nextHorizontal(const Pos p); //se n for valido ele retorna o mesmo valor
+    bool isLinValid();
+    bool isColValid();
+    bool isValid();
+    Pos nextVerticalCima();
+    Pos nextVerticalBaixo();
+    Pos nextHorizontalDireita();
+    Pos nextHorizontalEsquerda();
 
     void operator=(const Pos p);
-    bool operator==(const Pos p); //deve ser ajustado para que 'A' seja igual a 'a' por exemplo
+    bool operator==(const Pos p); 
 };
 
 #endif // POS_H
