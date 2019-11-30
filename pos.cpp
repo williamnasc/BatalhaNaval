ef958221bb9cd92e1f806720764082358273c9bb
JoAdrian <50709946+JoAdrian@users.noreply.github.com>
2019-11-29 22:23:47 -0300

    Mudança
    Implementação dos métodos e construtores da classe Pos

--- pos.cpp
+++ pos.cpp
@@ -1,57 +1,142 @@
+#include <bits/stdc++.h>
 #include "pos.h"
 
-Pos::Pos(char l, char c){
-    lin = l;
-    col = c;
-    if(!isValid(this)){
-        cerr << " coordenadas invalidas ";
-        lin = ' ';
-        col = ' ';
+Pos::Pos(string &s){
+    if(s.length() == 2){
+        unsigned n = s.length();
+        char c[n+1];
+        strcpy(c, s.c_str());
+        if((c[0] >= 65 && c[0] <= 74) && (c[1] >= 48 && c[1] <= 57)){
+            for(int i = 0; i < 10; i++){
+                if(c[0] == 65+i){
+                    lin = i;
+                    break;
+                }
+            }
+            for(int i = 0; i < 10; i++){
+                if(c[1] == 48+i){
+                    col = i;
+                    break;
+                }
+            }
+        }
+        else if((c[0] >= 97 && c[0] <= 106) && (c[1] >= 48 && c[1] <= 57)){
+            for(int i = 0; i < 10; i++){
+                if(c[0] == 97+i){
+                    lin = i;
+                    break;
+                }
+            }
+            for(int i = 0; i < 10; i++){
+                if(c[1] == 48+i){
+                    col = i;
+                    break;
+                }
+            }
+        }
     }
-
 }
 
-bool Pos::isValid(Pos p){
+Pos::Pos(char l, char c){
+    if((l >= 65 && l <= 74) && (c >= 48 && c <= 57)){
+        for(int i = 0; i < 10; i++){
+                if(l == 65+i){
+                    lin = i;
+                    break;
+                }
+            }
+        for(int i = 0; i < 10; i++){
+            if(c == 48+i){
+                col = i;
+                break;
+            }
+        }
+    }
+    else if((l >= 97 && l <= 106) && (c >= 48 && c <= 57)){
+            for(int i = 0; i < 10; i++){
+                if(l == 97+i){
+                    lin = i;
+                    break;
+                }
+            }
+            for(int i = 0; i < 10; i++){
+                if(c == 48+i){
+                    col = i;
+                    break;
+                }
+            }
+        }
+}
+
+bool Pos::isValid(){
     if(
-            isLinValid(p.lin) &&
-            isColValid(p.col)
+            isLinValid() &&
+            isColValid()
        )
-        return false;
-    return true;
+        return true;
+    return false;
 }
-bool Pos::isLinValid(char c){
-    if((c<'A' || c>'J'))
-        return false;
-    return true;
-}
-bool Pos::isColValid(char c){
-    if((c<'0' || c>'9'))
+
+bool Pos::isLinValid(){
+    if((lin<0 || lin>9))
         return false;
     return true;
 }
 
-Pos Pos::nextVertical(const Pos p){
-    char c = p.col;
-    if(isColValid(c++))
-        return Pos(p.lin,c++);
-    return p;
-}
-Pos Pos::nextHorizontal(const Pos p){
-    char c = p.lin;
-    if(isLinValid(c++))
-        return Pos(lin++,col);
-    return p;
+bool Pos::isColValid(){
+    if((col<0 || col>9))
+        return false;
+    return true;
 }
 
-void Pos::operator=(const Pos p){
+Pos Pos::nextVerticalCima(){
+    Pos p1(lin--, col);
+    if(p1.isValid()) return p1;
+    else {
+        Pos p2(lin, col);
+        return p2;
+    }
+}
+
+Pos Pos::nextVerticalBaixo(){
+    Pos p1(lin++, col);
+    if(p1.isValid()) return p1;
+    else {
+        Pos p2(lin, col);
+        return p2;
+    }
+}
+
+Pos Pos::nextHorizontalDireita(){
+    Pos p1(lin, col++);
+    if(p1.isValid()) return p1;
+    else {
+        Pos p2(lin, col);
+        return p2;
+    }
+}
+
+Pos Pos::nextHorizontalEsquerda(){
+    Pos p1(lin, col--);
+    if(p1.isValid()) return p1;
+    else {
+        Pos p2(lin, col);
+        return p2;
+    }
+}
+
+void Pos::operator=(const Pos& p){
     lin = p.lin;
     col = p.col;
 }
-bool Pos::operator==(const Pos p){
+
+bool Pos::operator==(const Pos& p){
     if((lin == p.lin)&&(col == p.col))
         return true;
     else {
-
+        return false;
     }
 }
+
+
 
